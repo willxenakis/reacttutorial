9e37a6aaca6474a05171d16321302e021091cf39
William Xenakis <william.xenakis@gmail.com>
2022-01-11 20:10:04 -0600

    Store Data

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,5 +1,24 @@
 import React from 'react';
-import { hasConflict, getCourseTerm, getCourseNumber} from '../utilities/times.js';
+import { timeParts, hasConflict, getCourseTerm, getCourseNumber} from '../utilities/times.js';
+import { setData } from '../utilities/firebase.js'
+
+const getCourseMeetingData = course => {
+    const meets = prompt('Enter meeting data: MTuWThF hh:mm-hh:mm', course.meets);
+    const valid = !meets || timeParts(meets).days;
+    if (valid) return meets;
+    alert('Invalid meeting data');
+    return null;
+  };
+
+  const reschedule = async (course, meets) => {
+    if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
+      try {
+        await setData(`/courses/${course.id}/meets`, meets);
+      } catch (error) {
+        alert(error);
+      }
+    }
+  };
 
 const Course = ({ course, selected, setSelected  }) => {
     const isSelected = selected.includes(course);
@@ -8,7 +27,8 @@ const Course = ({ course, selected, setSelected  }) => {
       backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
     };
     return (
-    <div className="card m-1 p-2" style={style} onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}>
+    <div className="card m-1 p-2" style={style} onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}
+    onDoubleClick={() => reschedule(course, getCourseMeetingData(course))}>
       <div className="card-body">
         <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
         <div className="card-text">{ course.title }</div>
--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -42,3 +42,7 @@ export const useData = (path, transform) => {
   
     return [data, loading, error];
   };
+
+  export const setData = (path, value) => (
+    set(ref(database, path), value)
+  );
